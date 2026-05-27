2c63b4adcfed61bc37444bc2389848310e01ec90
s4167575 <s4167575@student.rmit.edu.au>
2026-05-27 20:54:21 +1000

    Added getters and setters for Driver class and added needed operations to DriverRepository (WIP)

--- IntelligentBusSystem/src/main/java/org/example/Driver.java
+++ IntelligentBusSystem/src/main/java/org/example/Driver.java
@@ -7,4 +7,56 @@ public class Driver {
     private String licenseType; // Light, Medium, Heavy, PublicTransport
     private String address;
     private String birthdate;
+
+    public Driver(String driverID, String name, int experienceYears, String licenseType, String address, String birthdate) {
+        this.driverID = driverID;
+        this.name = name;
+        this.experienceYears = experienceYears;
+        this.licenseType = licenseType;
+        this.address = address;
+        this.birthdate = birthdate;
+    }
+
+    public String getDriverID() { 
+        return driverID; 
+    }
+    
+    public void setDriverID(String driverID) {
+        this.driverID = driverID;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getExperienceYears() {
+        return experienceYears;
+    }
+    public void setExperienceYears(int experienceYears) {
+        this.experienceYears = experienceYears;
+    }
+
+    public String getLicenseType() {
+        return licenseType;
+    }
+    public void setLicenseType(String licenseType) {
+        this.licenseType = licenseType;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
 }
