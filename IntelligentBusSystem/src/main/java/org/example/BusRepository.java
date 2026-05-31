a8e7ed5a3aca510e181d048c2746a22f5b0ba9f2
s4170411 <s4170411@student.rmit.edu.au>
2026-05-31 13:23:01 +1000

    Add update command

--- IntelligentBusSystem/src/main/java/org/example/BusRepository.java
+++ IntelligentBusSystem/src/main/java/org/example/BusRepository.java
@@ -1,25 +1,64 @@
 package org.example;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class BusRepository {
+
     // Add (), Update (), Retrieve (), Count () functions
+
+    // Grab list, add, re-write to JSON
     public void add(Bus bus) {
+        JSONParser parser = new JSONParser();
+        JSONArray busList = new JSONArray();
+        try (FileReader fileRead = new FileReader("busRepo.json")) {
+            busList = (JSONArray) parser.parse(fileRead);
+        } catch (Exception e) {
+            // Proceed with empty list
+        }
+        
         JSONObject jsonObject = new JSONObject();
 
         jsonObject.put("busID", bus.getBusID());
         jsonObject.put("capacity", bus.getCapacity());
         jsonObject.put("fuelType", bus.getFuelType());
         jsonObject.put("fuelLevel", bus.getFuelLevel());
-       
-        try {
-            FileWriter file = new FileWriter("busRepo.json");
-            file.write(jsonObject.toJSONString());
-            file.close();
+
+        busList.add(jsonObject);
+        try(FileWriter file = new FileWriter("busRepo.json")) {
+            file.write(busList.toJSONString());
+            file.flush();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to save bus to JSON", e);
         }
+
+    }
+
+    public void update(String busID) {
+
+        // Same reader as "add" method
+        JSONParser parser = new JSONParser();
+        JSONArray busList = new JSONArray();
+        try (FileReader fileRead = new FileReader("busRepo.json")) {
+            busList = (JSONArray) parser.parse(fileRead);
+        } catch (Exception e) {
+            throw new RuntimeException("No buses exist to update");
+        }
+
+
+
+    }
+
+    public void retrieve(Bus bus) {
+
+    }
+    
+    public void count (Bus bus) {
 
     }
 }
